55dc204867af6555b4c62f5bfb2c63ac6b149aeb
Jonathan Guthrie <jono@perfect.org>
2017-01-16 01:21:09 -0500

    Initial commit

---
+++ Package.swift
@@ -0,0 +1,11 @@
+// Generated automatically by Perfect Assistant Application
+// Date: 2017-01-13 19:07:26 +0000
+import PackageDescription
+let package = Package(
+    name: "PerfectTurnstileMongoDBDemo",
+    targets: [],
+    dependencies: [
+        .Package(url: "https://github.com/PerfectlySoft/Perfect-Turnstile-MongoDB.git", majorVersion: 0),
+        .Package(url: "https://github.com/PerfectlySoft/Perfect-RequestLogger.git", majorVersion: 1, minor: 0),
+    ]
+)
