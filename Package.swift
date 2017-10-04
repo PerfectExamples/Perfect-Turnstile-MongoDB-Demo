2e8e5626c5a39d6895fbd9d4db8fab7f6a456320
Jonathan Guthrie <jono@perfect.org>
2017-10-04 18:03:48 -0400

    perfect 3 swift 4

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ let package = Package(
     name: "PerfectTurnstileMongoDBDemo",
     targets: [],
     dependencies: [
-        .Package(url: "https://github.com/PerfectlySoft/Perfect-Turnstile-MongoDB.git", majorVersion: 1),
-        .Package(url: "https://github.com/PerfectlySoft/Perfect-RequestLogger.git", majorVersion: 1),
+        .Package(url: "https://github.com/PerfectlySoft/Perfect-Turnstile-MongoDB.git", majorVersion: 3),
+        .Package(url: "https://github.com/PerfectlySoft/Perfect-RequestLogger.git", majorVersion: 3),
     ]
 )
